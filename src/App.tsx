7e74947509c45bf06328686c9cd9f38fd929799e
goodbyebada <goodbyebada@naver.com>
2025-11-24 23:51:54 +0900

    chore: 빌드 환경 및 라우팅 설정 업데이트
    - Prettier 설정 개선 (quote 옵션 추가)
    - Husky pre-commit 헬퍼 스크립트 및 NVM 경로 설정
    - React Router 기본 라우팅 구조 추가
    - .gitignore에 .claude/commands 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import Test from './pages/Test';
 
 const Home = () => (
-  <div className="flex h-screen items-center justify-center">
-    <h1 className="text-4xl font-bold">DoLink Web Client</h1>
+  <div className='flex h-screen items-center justify-center'>
+    <h1 className='text-4xl font-bold'>DoLink Web Client</h1>
   </div>
 );
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Home />} />
 
       {/* 공통 컴포넌트 테스트용 페이지 */}
-      <Route path="/test" element={<Test />} />
+      <Route path='/test' element={<Test />} />
     </Routes>
   );
 }
