6636b482382a3335e6ae44108ec7004c619ab6cc
kimseohyeon <seohyeonidaya@gmail.com>
2025-12-24 13:18:57 +0900

    style: 고정 높이를 전체 너비로 변경

--- src/components/common/line/greyLine.tsx
+++ src/components/common/line/greyLine.tsx
@@ -3,8 +3,8 @@ interface GreyLineProps {
 }
 
 /**
- * 가로 375px, 세로 1px 회색 선 컴포넌트
+ * 가로 full, 세로 1px 회색 선 컴포넌트
  */
 export const GreyLine = ({ className = '' }: GreyLineProps) => {
-  return <div className={`h-[1px] w-[375px] bg-grey-200 ${className}`} />;
+  return <div className={`h-[1px] w-full bg-grey-200 ${className}`} />;
 };
