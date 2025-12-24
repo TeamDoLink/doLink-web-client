a40a85d0a4ade8b06c047a221c141b5550b76a9a
kimseohyeon <seohyeonidaya@gmail.com>
2025-12-24 13:33:39 +0900

    feat: 모음 개수 요약 바 컴포넌트 구현

---
+++ src/components/archive/archiveSummaryBar.tsx
@@ -0,0 +1,39 @@
+import type { ButtonHTMLAttributes, HTMLAttributes } from 'react';
+import plusIcon from '@/assets/icons/common/plus.svg';
+import { GreyLine } from '@/components/common/line/greyLine';
+
+interface ArchiveSummaryBarProps extends HTMLAttributes<HTMLDivElement> {
+  totalCount: number;
+  onClickAdd?: ButtonHTMLAttributes<HTMLButtonElement>['onClick'];
+}
+
+/**
+ * 아카이브 개수 요약 바
+ * - 좌측: 총 n개
+ * - 우측: 모음 추가 버튼
+ */
+export const ArchiveSummaryBar = ({
+  totalCount,
+  onClickAdd,
+  className = '',
+  ...divProps
+}: ArchiveSummaryBarProps) => {
+  return (
+    <div className={`w-full px-5 ${className}`} {...divProps}>
+      <div className='flex h-[43px] w-full items-center justify-between'>
+        <p className='text-body-md text-grey-900'>
+          총 <span className='text-point'>{totalCount}</span>개
+        </p>
+        <button
+          type='button'
+          onClick={onClickAdd}
+          className='flex items-center gap-1 text-body-md text-grey-700'
+        >
+          <img src={plusIcon} alt='' aria-hidden='true' className='h-4 w-4' />
+          <span>모음 추가</span>
+        </button>
+      </div>
+      <GreyLine />
+    </div>
+  );
+};
