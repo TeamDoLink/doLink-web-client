edd784a5fe6c9af9d1e8d14ffdc093438f7c225a
seohyeon0219 <seohyeonidaya@gmail.com>
2026-01-04 04:04:20 +0900

    refactor: SettingMenuItem props optional 처리 및 렌더링 정리

--- src/components/common/setting/settingAppBar.tsx
+++ src/components/common/setting/settingAppBar.tsx
@@ -3,9 +3,9 @@ import rightIcon from '@/assets/icons/common/direction-right.svg';
 
 interface SettingMenuItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   // 왼쪽 텍스트
-  leftText: string;
+  leftText?: string;
   // 오른쪽 텍스트
-  rightText: string;
+  rightText?: string;
   // 화살표 아이콘 표시 여부 (기본값: true)
   showArrow?: boolean;
 }
@@ -23,17 +23,19 @@ export const SettingMenuItem = ({
   className = '',
   ...props
 }: SettingMenuItemProps) => {
+  const renderRight = rightText ? (
+    <span className='text-body-md text-grey-500'>{rightText}</span>
+  ) : null;
+
   return (
     <button
       type='button'
       className={`flex h-14 w-full items-center justify-between bg-white px-[20px] py-[10px] text-left ${className}`}
       {...props}
     >
-      <span className='text-heading-md text-grey-900'>{leftText}</span>
+      <span className='text-heading-md text-grey-900'>{leftText ?? ''}</span>
       <div className='flex items-center gap-1'>
-        {rightText && (
-          <span className='text-body-md text-grey-500'>{rightText}</span>
-        )}
+        {renderRight}
         {showArrow && <img src={rightIcon} alt='다음' className='h-4 w-4' />}
       </div>
     </button>
