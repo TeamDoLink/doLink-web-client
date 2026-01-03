5ff66d79ce47710e15439c500d9e7a72b914413f
seohyeon0219 <seohyeonidaya@gmail.com>
2026-01-04 06:15:20 +0900

    feat: login page 라우팅 경로 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from '@/pages/home/homePage';
 import ArchivePage from '@/pages/archive/archivePage';
 import SettingsPage from '@/pages/settings/settingsPage';
+import LoginPage from '@/pages/auth/loginPage';
 import Test from '@/pages/test';
 import Test2 from '@/pages/test2';
 
@@ -12,6 +13,7 @@ const AppRouter = () => {
       <Route path='/' element={<HomePage />} />
       <Route path='/archives' element={<ArchivePage />} />
       <Route path='/settings' element={<SettingsPage />} />
+      <Route path='/login' element={<LoginPage />} />
 
       {/* 공통 컴포넌트 테스트 페이지 */}
       <Route path='/test' element={<Test />} />
