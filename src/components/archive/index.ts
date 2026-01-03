21d37f5a46cadf1e03b866a48b7a5c745c3450ff
seohyeon0219 <seohyeonidaya@gmail.com>
2026-01-03 15:49:37 +0900

    fix: archive 컴포넌트 export 수정

--- src/components/archive/index.ts
+++ src/components/archive/index.ts
@@ -1,7 +1,8 @@
 export { ArchiveBottomSheet } from './archiveBottomSheet';
 export { ArchiveInput } from './archiveInput';
 export { ArchiveSelect } from './archiveSelect';
-export type { ArchiveCategoryKey } from './archiveSelect';
-
-export * from './categoryFilterButton';
-export * from './archiveSummaryBar';
+export {
+  CategoryFilterButton,
+  type ArchiveCategoryKey,
+} from './categoryFilterButton';
+export { ArchiveSummaryBar } from './archiveSummaryBar';
