93b97b73267db29d114304d80674b85f3cbe2b24
seohyeon0219 <seohyeonidaya@gmail.com>
2026-01-14 22:06:49 +0900

    feat: 로딩 스피너 접근성 개선

--- src/components/common/loading/loadingSpinner.tsx
+++ src/components/common/loading/loadingSpinner.tsx
@@ -1,5 +1,7 @@
 export const LoadingSpinner = () => {
-  return <span className='loading-spinner' />;
+  return (
+    <span className='loading-spinner' role='status' aria-label='로딩 중' />
+  );
 };
 
 export default LoadingSpinner;
