4376eef1561f0a105731a8d2f71d077ee26f73fa
seohyeon0219 <seohyeonidaya@gmail.com>
2026-01-15 01:05:24 +0900

    chore: 라인 컴포넌트 접근성 속성 추가

--- src/components/common/line/greyVerticalLine.tsx
+++ src/components/common/line/greyVerticalLine.tsx
@@ -6,5 +6,11 @@ interface GreyVerticalLineProps {
  * 세로 48px, 두께 2px 회색 선 컴포넌트
  */
 export const GreyVerticalLine = ({ className = '' }: GreyVerticalLineProps) => {
-  return <div className={`h-12 w-0.5 bg-grey-300 ${className}`} />;
+  return (
+    <div
+      className={`h-12 w-0.5 bg-grey-300 ${className}`}
+      role='presentation'
+      aria-hidden='true'
+    />
+  );
 };
