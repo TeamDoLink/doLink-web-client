8e010ee9d5f58214522e88f941fe3c712e70b7d4
seohyeon0219 <seohyeonidaya@gmail.com>
2026-02-17 02:31:21 +0900

    feat: 홈 화면 할 일 클릭 시 상세 페이지 이동
    - handleTaskClick 핸들러 추가
    - TodoSection에 onTaskClick props 전달

--- src/pages/home/afterLogin.tsx
+++ src/pages/home/afterLogin.tsx
@@ -215,6 +215,10 @@ const HomeAfterLogin = () => {
     navigate(ROUTES.search);
   };
 
+  const handleTaskClick = (id: string) => {
+    navigate(`${ROUTES.taskDetail}/${id}`);
+  };
+
   return (
     <div className='relative flex min-h-screen flex-col'>
       <Background.GradientBackground>
@@ -225,7 +229,11 @@ const HomeAfterLogin = () => {
         <main className='relative grow'>
           <div className='mx-auto flex flex-col px-5 py-2'>
             <GreetingSection memberName={memberName} greeting={greeting} />
-            <TodoSection items={latestTodos} onToggle={handleToggleTodo} />
+            <TodoSection
+              items={latestTodos}
+              onToggle={handleToggleTodo}
+              onTaskClick={handleTaskClick}
+            />
             <ArchiveSection
               items={latestArchives}
               onRequestDelete={handleRequestDeleteArchive}
