4392f9619ccf49889003ea799d3fdd8f70fddda2
dexical30 <dexical30@gmail.com>
2026-03-13 17:34:54 +0900

    feat: ReactNativeWebview를 모킹하여 웹에서도 테스트 가능한 구조로 개발

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@/styles/global.css';
+import { initMockReactNativeWebView } from '@/utils/MockReactNativeWebView';
 import App from './App';
+
+initMockReactNativeWebView();
 
 const queryClient = new QueryClient({
   defaultOptions: {
